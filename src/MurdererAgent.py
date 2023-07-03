282e66d0482835f4a42e58f2e70cc56d8ae38a5e
João Victor <joao15victor08@gmail.com>
2023-07-02 22:27:05 -0300

    Adiciona agente assassino
    Co-authored-by: Thiago Lopes <thiago.lopes.santos.tls@gmail.com>
    Co-authored-by: Thiago Gomes <thiagosiqgomes@gmail.com>

--- src/MurdererAgent.py
+++ src/MurdererAgent.py
@@ -1,14 +1,12 @@
 from mesa import Agent
 
 
-class CharacterAgent(Agent):
-
-    def __init__(self, unique_id, model, role="Survivor", moore=True, view_range=10, walk_speed=1):
+class MurdererAgent(Agent):
+    def __init__(self, unique_id, model, view_range=10,walk_speed=2, moore=True):
         super().__init__(unique_id, model)
-        self.role = role  # Papel do personagem: Survivor or Murderer
-        self.moore = moore  # Personagem só anda na vertical e horizontal
         self.view_range = view_range  # Alcance da visão do personagem
-        self.walk_speed = walk_speed # Quadrados andados por passo('velocidade')
+        self.walk_speed = walk_speed  # Quadrados andados por passo('velocidade')
+        self.moore = moore  # Personagem só anda na vertical e horizontal
 
     def step(self):
         if self.model.game_over:
@@ -16,6 +14,7 @@ class CharacterAgent(Agent):
         self.walk()
 
     def walk(self):
+
         possible_walk_pos = self.model.grid.get_neighborhood(
             self.pos, moore=self.moore, include_center=True, radius=self.view_range)
         new_position = self.get_new_position(
@@ -29,11 +28,12 @@ class CharacterAgent(Agent):
         minimum_distance = 10**6
         step_x = 0
         step_y = 0
-        # Para cada posição possível de andar, ele procura o gerador
+        # Para cada posição possível de andar, ele procura um sobrevivente
         for walk_pos in possible_walk_pos:
-            generator = self.check_pos_for_generator(walk_pos)
+
+            survivor = self.check_pos_for_survivor(walk_pos)
             # Se tiver um gerador ao alcance
-            if len(generator) > 0:
+            if len(survivor) > 0:
                 # Calcula distância entre o gerador e a posição atual
                 current_distance = (
                     current_pos[0] - walk_pos[0])**2 + (current_pos[1] - walk_pos[1])**2
@@ -64,12 +64,19 @@ class CharacterAgent(Agent):
         next_pos = self.model.grid.get_neighborhood(
             self.pos, moore=self.moore, include_center=False, radius=self.walk_speed)
         return self.random.choice(next_pos)
-
-    def check_pos_for_generator(self, pos):
+    
+    def check_pos_for_survivor(self, pos):
         try:
             # Retorna lista de objetos na célula diferentes de Agent
             this_cell = self.model.grid.get_cell_list_contents([pos])
-            return [obj for obj in this_cell if not isinstance(obj, CharacterAgent)]
+            target = []
+            # print("AQUI POSIÇAO ATUAL ", this_cell)
+            for obj in this_cell:
+                if obj in self.model.survivors:
+                    target.append(obj)
+                # print("TESTE POSICAO: ", target)
+            return target
         except:
             # Retorna nada
+            print("ERRO AO PROCURAR POSICAO")
             return []
