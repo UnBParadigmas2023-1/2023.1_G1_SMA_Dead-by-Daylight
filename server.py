5cce0650380c3dcfbf6971264dc6384267e0a799
gustavoduartemoreira <gustavoduartemoreira@gmail.com>
2023-07-03 00:31:32 -0300

    add guy fawkes mask icon

--- server.py
+++ server.py
@@ -14,7 +14,7 @@ def agent_portrayal(agent):
             portrayal['Shape'] = "assets/light_off.png"
     elif type(agent) is SurvivorAgent:
         if agent.alive:
-            portrayal['Shape'] = "assets/blob.png"
+            portrayal['Shape'] = "assets/v.png"
     elif type(agent) is MurdererAgent:
         portrayal['Shape'] = "assets/jason_mask.png"
     elif type(agent) is ExitAgent:
